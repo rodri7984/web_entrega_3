365f1720b32f7f4279d22b629929b408abf5e236
rodri <rodri@gmail.com>
2023-06-21 21:08:02 -0400

    BD vacia

--- productos/admin.py
+++ productos/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Marca, Talla, Producto
 
 # Register your models here.
+
+admin.site.register(Marca)
+admin.site.register(Talla)
+admin.site.register(Producto)
--- productos/models.py
+++ productos/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+class Marca(models.Model):
+    id_marca  = models.AutoField(db_column='idMarca', primary_key=True) 
+    marca     = models.CharField(max_length=20, blank=False, null=False)
+
+    def _str_(self):
+        return str(self.marca)
+
+class Talla(models.Model):
+    id_talla  = models.AutoField(db_column='idTalla', primary_key=True) 
+    talla     = models.CharField(max_length=20, blank=False, null=False)
+
+    def _str_(self):
+        return str(self.talla)
+
+
+class Producto(models.Model):
+    id_prod          = models.CharField(primary_key=True, max_length=10)
+    nombre_prod      = models.CharField(max_length=20)
+    id_marca         = models.ForeignKey('Marca',on_delete=models.CASCADE, db_column='idMarca')  
+    id_talla         = models.ForeignKey('Talla',on_delete=models.CASCADE, db_column='idTalla')
+    color            = models.CharField(max_length=20)  
+    precio           = models.IntegerField()
+    imagen           = models.ImageField(upload_to="productos", null=True)
+
+    def _str_(self):
+        return str(self.nombre_prod)+" "+str(self.color)+" "+str(self.precio)
