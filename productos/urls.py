883536b81cb0346352fcab20b9edd5e2b530db3a
rodri <rodri@gmail.com>
2023-06-23 15:24:45 -0400

    crud completo

--- productos/urls.py
+++ productos/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('index', views.index, name='index'),
     path('productosList', views.productosList, name="productosList"),
+    path('productosAdd', views.productosAdd, name="productosAdd"),
+    path('productosEdit/<str:pk>', views.productosEdit, name="productosEdit"),
+    path('productosDel/<str:pk>', views.productosDel, name="productosDel"),
     
 ]
--- productos/views.py
+++ productos/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Marca, Talla, Producto
+from .forms import ProductoForm
 # Create your views here.
 
 def index(request):
@@ -12,3 +13,34 @@ def productosList(request):
         'productos' : productos
     }
     return render(request, 'productos/productosList.html',context)
+
+def productosAdd(request):
+    context= {'form': ProductoForm()}
+    #verificar peticion Post
+    if request.method=='POST':
+        #con el request se recuperan los datos del form
+        formulario=ProductoForm(request.POST, files=request.FILES)
+        if formulario.is_valid:
+            formulario.save()
+            context={'mensaje':"Producto guardado correctamente"}
+    return render(request, 'productos/productosAdd.html', context)
+
+def productosEdit(request, pk):
+    # Select * from alumnos where id_prod=pk
+    producto=Producto.objects.get(id_prod=pk)
+
+    context= {'form': ProductoForm(instance=producto)}
+    #verificar peticion Post
+    if request.method=='POST':
+        #con el request se recuperan los datos del form
+        # se filtra por el id del producto a editar
+        formulario=ProductoForm(request.POST, files=request.FILES, instance=producto)
+        if formulario.is_valid:
+            formulario.save()
+            context={'mensaje':"Producto se edito correctamente"}
+    return render(request, 'productos/productosAdd.html', context)
+
+def productosDel(request,pk):
+    producto=Producto.objects.get(id_prod=pk)
+    producto.delete()
+    return redirect(to='productosList')
