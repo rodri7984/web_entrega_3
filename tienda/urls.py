ecb0c83dccb47589af40f018719ae90473279184
rodri <rodri@gmail.com>
2023-06-24 14:22:00 -0400

    prueba de rama

--- tienda/urls.py
+++ tienda/urls.py
@@ -21,7 +21,8 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('productos/', include('productos.urls'))
+    path('productos/', include('productos.urls')),
+    path('administrador/', include('administrador.urls')),
 ]
 
 if settings.DEBUG:
